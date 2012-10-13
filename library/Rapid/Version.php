33a976573d3a79f45d30345f0f4c275661d6616f
Dmitry Merkushin <merkushin@gmail.com>
2012-10-13 17:39:10 +0400

    added \Rapid\Version i use it just for LoaderTest :) but i think it would be useful in the future :)

---
+++ library/Rapid/Version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package Rapid
+ * @author Dmitry Merkushin <merkushin@gmail.com>
+ */
+
+namespace Rapid;
+
+class Version
+{
+    public static function get()
+    {
+        return '0.12.0';
+    }
+}
